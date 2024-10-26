ad2169adb35bc1335b035d63cf2fae57e9a61ac1
Mysticdrew <drewhaas@gmail.com>
2024-10-26 11:59:03 -0500

    1.21.1-1.0.2

--- Common/src/main/kotlin/journeymap_webmap/service/webmap/kotlin/routes/skin.kt
+++ Common/src/main/kotlin/journeymap_webmap/service/webmap/kotlin/routes/skin.kt
@@ -14,17 +14,17 @@ internal fun skinGet(ctx: Context)
     val uuid = UUID.fromString(ctx.pathParam("uuid"))
     val profile = Minecraft.getInstance().connection?.getPlayerInfo(uuid)?.profile
 
-    val img: NativeImage = if (profile == null)
+    val img: NativeImage? = if (profile == null)
     {
         NativeImage(24, 24, false)
     }
     else
     {
-        IgnSkin.getFaceImage(profile)
+        IgnSkin.getFace(profile).pixels
     }
 
     ctx.contentType(ContentType.IMAGE_PNG)
-    img.writeToChannel(Channels.newChannel(ctx.outputStream()))
+    img?.writeToChannel(Channels.newChannel(ctx.outputStream()))
     ctx.outputStream().flush()
-    img.close()
+    img?.close()
 }
